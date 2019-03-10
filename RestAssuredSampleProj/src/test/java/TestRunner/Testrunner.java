76b2e0163258ca2b7f04a2c1265b0a46d366cbf2
harishs2014 <39235147+harishs2014@users.noreply.github.com>
2019-03-10 23:32:56 +0530

    Update Testrunner.java
    Test

--- RestAssuredSampleProj/src/test/java/TestRunner/Testrunner.java
+++ RestAssuredSampleProj/src/test/java/TestRunner/Testrunner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 	    strict=true,
 	    dryRun=false,
 		tags= {"@DemoTest"},
-	        plugin = { "pretty", "html:target/cucumber-reports" },
+	         plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json","junit:target/cucumber-reports/Cucumber.xml","html:target/cucumber-reports"},
 		glue={""} 
 		)
 
